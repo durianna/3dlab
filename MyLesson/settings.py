95d41d36c1fb16d7e9492d5932629f651659fac3
VasyaTarnavsky <cumpuellavas@gmail.com>
2025-04-20 18:01:33 +0300

    add static

--- MyLesson/settings.py
+++ MyLesson/settings.py
@@ -140,10 +140,9 @@ LOCALE_PATHS = [
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+STATICFILES_DIRS = []
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = 'home'
